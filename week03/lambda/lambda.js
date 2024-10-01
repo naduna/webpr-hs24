e000df4b6711531e92e37fa91130464d3b381f94
Nadine Zbinden <nadine.zbinden@students.fhnw.ch>
2024-10-01 17:10:26 +0200

    fix ball play and lamba kakül stuff

--- week03/lambda/lambda.js
+++ week03/lambda/lambda.js
@@ -1,6 +1,39 @@
+const id = x => x;
 
+const konst = x => y => x;
 
+const kite = x => y => y;
 
+// const T = konst;
+// const F = kite;
+
+const T = tc => fc => tc;
+const F = tc => fc => fc;
+
+// a (ausgewertet wenn a == true) (ausgewertet wenn a == false)
+// heisst wenn a == true, kann es true werden als Ganzes, indem b true ist.
+// wenn a == false, kann es als Ganzes nicht mehr true werden, also eh False
+// const and = a => b => a(b(T)(F))(b(F)(F)); // verkürzt siehe unten
+// const and = a => b => a(b)(F); // a ist hinten eh false, also kann man direkt a zurückgeben
+const and = a => b => a(b)(a);
+
+const or = a => b => a(T)(b)
+
+const flip = f => x => y => f(y)(x)
+
+const Pair = fn => ln => fnOrLn => fnOrLn(fn)(ln);
+const firstname = konst;
+const lastname = kite;
+
+const Left = x => f => g => f(x);
+const Right = x => f => g => g(x);
+// const either = eitherLeftOrRight => f => g => eitherLeftOrRight(f)(g) // eta reduction leads to below
+// const either = eitherLeftOrRight => f => eitherLeftOrRight(f) // eta reduction leads to below
+// const either = eitherLeftOrRight => f => eitherLeftOrRight(f) // eta reduction leads to below
+// const either = eitherLeftOrRight => eitherLeftOrRight // alpha translation leads to below
+// const either = x => x // beta reduction leads to below
+const either = id
+    
 // ----- special -----
 
 const Tuple = n => [
--- week2/ball/ball.js
+++ week2/ball/ball.js
@@ -1,7 +1,10 @@
 
 const radius = 10;
-const ball = {x:10, y:1, dx: 5, dy: 1};
+const ball = {x: 10, y:1, dx: 5, dy: 1};
 let   old  = {x: ball.x, y: ball.y};
+
+const outOfBoundsMax = 400 - radius;
+const outOfBoundsMin = radius;
 
 function start() {
     const canvas  = document.getElementById("canvas");
@@ -10,7 +13,6 @@ function start() {
 
     setInterval(() => {
         if (Math.abs(ball.dx) < 0.1 && Math.abs(ball.dy) < 0.1) return;
-        if (Math.abs(ball.dx) > 50 && Math.abs(ball.dy) > 50) return;
         nextBoard();
         display(context);
     }, 1000 / 20);
@@ -18,31 +20,37 @@ function start() {
 
 function nextBoard() {
     // keep old ball values for the sake of efficient clearing of the old display
-    old = ball;
+    old = {...ball};
 
     // handle ball is hitting the bounds
     //   reverse direction
     //   lose some energy relative to the current inertia (only velocity varies)
-    if (old.x === 400 || old.y === 400){
-        ball.dx = -old.dx;
-        ball.dy = -old.dy;
+    const hitBottom = old.y >= outOfBoundsMax && ball.dy > 0;
+    const hitTop = old.y <= outOfBoundsMin && ball.dy < 0;
+    const hitRight = old.x >= outOfBoundsMax && ball.dx > 0;
+    const hitLeft = old.x <= outOfBoundsMin && ball.dx < 0;
+    
+    if (hitBottom || hitTop){
+        ball.dy -= 5;
+        ball.dy = -ball.dy;
     }
-    // if (old.x === 400){
-    //     ball.dx = -old.dx;
-    // }
-    // if (old.y === 400){
-    //     ball.dy = -old.dy;
-    // }
+    
+    if (hitRight || hitLeft){
+        ball.dx = -ball.dx;
+        ball.dx *= 0.8;
+    }
 
     // calculate new position
     // calculate any changes in velocity due to gravitational pull or medium resistance
-    ball.x = old.x + old.dx;
-    ball.y = old.y + old.dy;
+    ball.x += ball.dx;
+    ball.y += ball.dy;
 
-    ball.dx = old.dx + 1;
-    ball.dy = old.dy + 1;
-    
-    console.log("is moving")
+    ball.dy += 1.5 ;      // constant acceleration
+
+    ball.y = Math.min(outOfBoundsMax, ball.y);
+    ball.x = Math.min(outOfBoundsMax, ball.x);
+    ball.y = Math.max(outOfBoundsMin, ball.y);
+    ball.x = Math.max(outOfBoundsMin, ball.x);
 }
 
 function display(context) {
